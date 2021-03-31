7bb978de411dee2b6e10bbe95f3695b3d74fff6c
danielhuskey <daniel.huskey@icloud.com>
2021-03-31 13:20:25 -0400

    channged from using ard to using GPIO on Pi

--- plane-modual/plane.py
+++ plane-modual/plane.py
@@ -2,12 +2,24 @@
 import socket
 import time
 import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
 ip = '192.168.1.90'
 port = 2222
 servoPIN0 = 14
-servoPIN1 = 24
+servoPIN1 = 15
 servoPIN2 = 16
 servoPIN3 = 26
+
+
+
+def gpio_setup(pin0, pin1, pin2, pin3):
+    
+    GPIO.setup(pin0, GPIO.OUT)
+    GPIO.setup(pin1, GPIO.OUT)
+    GPIO.setup(pin2, GPIO.OUT)
+    GPIO.setup(pin3, GPIO.OUT)
+gpio_setup(servoPIN0,servoPIN1,servoPIN2,servoPIN3)
+
 
 servo0 = GPIO.PWM(servoPIN0, 50)
 servo1 = GPIO.PWM(servoPIN1, 50)
@@ -19,13 +31,8 @@ servo1.start(2.5)
 servo2.start(2.5)
 servo3.start(2.5)
 
-def gpio_setup(pin0, pin1, pin2, pin3):
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(pin0, GPIO.OUT)
-    GPIO.setup(pin1, GPIO.OUT)
-    GPIO.setup(pin2, GPIO.OUT)
-    GPIO.setup(pin3, GPIO.OUT)
-    
+
+
 def servoTest():
     # moving the servos to make sure its working
     servo0.ChangeDutyCycle(2+(0/18))
@@ -65,30 +72,33 @@ def connect_to_ground_server():
     connectServer.groundServer.close
     connectServer.groundServer.connect((ip,  port))
 
-def sepperating_commands():
-    #sepperting the commands to the plane into their own parts
+def write_to_servo():    
     total_Controler_Input = connectServer.groundServer.recv(1024)
     controller_Input_Spilt = total_Controler_Input.split(',')
-    motor0Power.sepperating_commands = controller_Input_Spilt[0]
-    motor1Power.sepperating_commands = controller_Input_Spilt[1]
-    servo0angle.sepperating_commands = controller_Input_Spilt[2]
-    servo1angle.sepperating_commands = controller_Input_Spilt[3]
-    servo2angle.sepperating_commands = controller_Input_Spilt[4]
-    servo3angle.sepperating_commands = controller_Input_Spilt[5]
+    
+    servo0angle = controller_Input_Spilt[0]
+    servo1angle = controller_Input_Spilt[1]
+    servo2angle = controller_Input_Spilt[2]
+    servo3angle = controller_Input_Spilt[3]
 
-def write_to_servo()    
-    servo0.ChangeDutyCycle(2+(servo0angle.sepperating_commands/18))
-    servo1.ChangeDutyCycle(2+(servo1angle.sepperating_commands/18))
-    servo2.ChangeDutyCycle(2+(servo2angle.sepperating_commands/18))
-    servo3.ChangeDutyCycle(2+(servo3angle.sepperating_commands/18))
-
+    servo0.ChangeDutyCycle(2+(int(servo0angle)/18))
+    servo1.ChangeDutyCycle(2+(int(servo1angle)/18))
+    servo2.ChangeDutyCycle(2+(int(servo2angle)/18))
+    servo3.ChangeDutyCycle(2+(int(servo3angle)/18))
+    time.sleep(.2)
+    servo0.ChangeDutyCycle(0)
+    servo1.ChangeDutyCycle(0)
+    servo2.ChangeDutyCycle(0)
+    servo3.ChangeDutyCycle(0)
+    
 #one time tests/ connections
-gpio_setup(servoPIN0,servoPIN1,servoPIN2,servoPIN3)
-serialconnect()
+
+
 servoTest()
 connectServer()
 
     
 while True:
-    servoSend()
+    write_to_servo()
+
 
